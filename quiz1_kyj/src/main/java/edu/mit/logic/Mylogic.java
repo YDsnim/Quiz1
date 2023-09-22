c44137d5642497782731e43a38795023d76f6305
YD <YD@DESKTOP-QEPIJOQ>
2023-09-22 15:48:02 +0900

    Mylogic 노드 준비

--- quiz1_kyj/src/main/java/edu/mit/logic/Mylogic.java
+++ quiz1_kyj/src/main/java/edu/mit/logic/Mylogic.java
@@ -7,17 +7,20 @@ public class Mylogic {
 
 	public static int addUp(int num1,int num2) {
 		//num1 부터 num2까지 모두 더한값 구하기
+		System.out.println("addup 진입");
 		int sum = 0;
 		for (int i = num1; i <= num2; i++) {
 			
 			 sum =+ i;
 		}
 		
+		System.out.println("addup 리턴");
 		return sum  ;
 	}
 	
 	public static int evenAddUp(int num1,int num2) {
 		//num1 부터 num2까지의 숫자중 짝수만 더할것
+		System.out.println("evenaddup 진입");
 		int sum = 0;
 		
 		for (int i = num1; i <= num2; i++) {
@@ -25,6 +28,7 @@ public class Mylogic {
 			if(i/2==0){sum=+i;}
 		
 		}
+		System.out.println("evenaddup 리턴");
 		return sum;
 		
 	}
