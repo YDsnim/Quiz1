ebf2fbdfa18a9c7e25017143c1534d06caedc10b
YD <YD@DESKTOP-QEPIJOQ>
2023-09-22 10:16:57 +0900

    [모범답안 https://github.com/xguest7/quiz1]
    1.깃허브 저장소 생성
     1-1. 깃허브 저장소 (quiz1)
     1-2 readme 생성
     1-3. 공개, 라이센스 mit, .gitignore 는 java로 셋팅
    ---------------------------------------------------------
    2.이클립스
     생성한 깃허브 저장소를 로컬 저장소에서 복사
    ---------------------------------------------------------
    3.스프링 mvc 프로젝트 생성 (quiz1_영문이니셜) edu.mit.controller
    3-1. 스프링프레임워크 -> 5.0.7
          자바  -> 11
          롬복 +1.18.28
          log4j -> 1.2.17 (어디서나 사용할수 있도록)
          junit -> 4.12
          servlet -> 3.1.0
    =>push
    --------------------------------------------------------------
    4. /quiz/quiz1 -> /quiz/quiz1.jsp (jsp내용은 controller에서 오늘은 금요일 이라고 보내온
    문자를 보여주는 화면)
     contoller는 QuizController로 명명 <<<<<-현위치
    -------------------------------------------------------

---
+++ quiz1_kyj/src/main/java/edu/mit/controller/QuizController.java
@@ -0,0 +1,20 @@
+package edu.mit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.log4j.Log4j;
+@Controller
+@Log4j
+
+
+public class QuizController {
+
+@GetMapping("/quiz/quiz1")
+	public void GotoQuiz1 () {
+			
+		log.info("quiz컨트롤러 /quiz/quiz1 요청 받음");
+//		예상결과= quiz/quiz1.jsp로 가는거지
+	}
+	
+}
