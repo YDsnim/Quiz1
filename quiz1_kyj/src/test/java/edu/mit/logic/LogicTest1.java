76bd2113a3873e4f397c9255a1c0a326f1a2ed9b
YD <YD@DESKTOP-QEPIJOQ>
2023-09-22 16:38:34 +0900

    6번 7번 테스트 의도에 맞게 수정

--- quiz1_kyj/src/test/java/edu/mit/logic/LogicTest1.java
+++ quiz1_kyj/src/test/java/edu/mit/logic/LogicTest1.java
@@ -2,6 +2,8 @@ package edu.mit.logic;
 
 import org.junit.Test;
 
+import lombok.extern.log4j.Log4j;
+@Log4j
 //@RunWith()
 public class LogicTest1 {
 
@@ -10,6 +12,30 @@ public void logicT1() {
 	System.out.println(Mylogic.addUp(1, 10));	
 	
 	}
+	
+	//출제자의 의도는 Log4j를 활용해서 더 간단하게 작성해보는 것
+	
+	@Test
+	public void logicT1_1(){
+		log.info("결과 "+Mylogic.addUp(1, 10));
+//	INFO : edu.mit.logic.LogicTest1 - 55
+	
+	}
+	@Test
+	public void logicT1_2(){
+		log.info("결과 "+Mylogic.evenAddUp(1, 10));
+		
+/* evenaddup 진입
+		2
+		4
+		6
+		8
+		10
+		evenaddup 리턴
+		INFO : edu.mit.logic.LogicTest1 - 30
+*/
+	}
+	
 	
 	
 	
--- quiz1_kyj/src/test/java/edu/mit/logic/LogicTest2.java
+++ quiz1_kyj/src/test/java/edu/mit/logic/LogicTest2.java
@@ -2,15 +2,28 @@ package edu.mit.logic;
 
 import org.junit.Test;
 
-//@RunWith()
+import lombok.extern.log4j.Log4j;
+
+@Log4j
 public class LogicTest2 {
 
+	
+	//출제자의 의도는 Log4j를 활용해서 더 간단하게 작성하며
+	//static이 아닌경우도 자바처럼 선언하고 활용이 가능하다는점	
+	//log.info를 쓰면 콘솔에 입력가능==sysout 필요x
+
 	@Test
-public void logicT2() {
-	System.out.println(Mylogic.evenAddUp(1, 10));	
+	public void logicT2_2() {
+		Mylogic2 mylogic2 = new Mylogic2();
+			
+		log.info(mylogic2.addUp(1, 10));
+		log.info(mylogic2.evenAddUp(1, 10));
+			
+			
+		}
+		
+		
+	
 	
 	}
-	
-	
-	
-}
+
